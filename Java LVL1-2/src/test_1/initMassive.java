36593aac435560fe9a4608f7bc6ed14a6346a672
Maxim <maks.voley@gmail.com>
2017-06-15 00:13:57 +0300

    1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. * С помощью цикла и условия заменить 0 на 1, 1 на 0;

---
+++ Java LVL1-2/src/test_1/initMassive.java
@@ -0,0 +1,17 @@
+package test_1;
+
+import java.util.Arrays;
+
+/**
+ * 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+ * С помощью цикла и условия заменить 0 на 1, 1 на 0;
+ */
+public class initMassive {
+    int[] massive = new int[10];
+    public void printMassive() {
+        for (int i = 0; i < massive.length; i++) {
+            massive[i] = (int) Math.round(Math.random() * 1);
+        }
+        System.out.println(Arrays.toString(massive));
+    }
+}
